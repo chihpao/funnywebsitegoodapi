ec4b07571f2cb6b636ff882d6206ce0079bdda2f
chihpao <chihpaoo@gmail.com>
2024-09-09 11:50:34 +0800

    調整手機排版

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -5,9 +5,9 @@ function HomeContent() {
   return (
     <div className="text-center">
       <h2 className="text-3xl mb-4">Welcome to My Funny Website</h2>
-      <div className="space-x-4">
-        <ButtonLink to="/joke">View Random Joke</ButtonLink>
-        <ButtonLink to="/meme">View Random Meme</ButtonLink>
+      <div className="space-y-4 sm:space-y-0 sm:space-x-4">
+        <ButtonLink to="/joke" className="block sm:inline-block">View Random Joke</ButtonLink>
+        <ButtonLink to="/meme" className="block sm:inline-block">View Random Meme</ButtonLink>
       </div>
     </div>
   );
