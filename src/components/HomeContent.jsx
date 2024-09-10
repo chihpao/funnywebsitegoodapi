6f2c9ea07ffb0cbc51167956fce04d5bac06ba4a
chihpao <chihpaoo@gmail.com>
2024-09-10 18:04:34 +0800

    修改 NavBar.jsx，將標題變成可點擊的連結

--- src/components/HomeContent.jsx
+++ src/components/HomeContent.jsx
@@ -3,14 +3,15 @@ import ButtonLink from './ButtonLink';
 
 function HomeContent() {
   return (
-    <div className="text-center">
-      <h2 className="text-3xl mb-4">Welcome to My Funny Website</h2>
-      <div className="space-y-4 sm:space-y-0 sm:space-x-4">
+    <div className="text-center responsive-bg">
+      <h2 className="responsive-text mb-4">Welcome to My Funny Website</h2>
+      <div className="button-container">
         <ButtonLink to="/joke" className="block sm:inline-block">View Random Joke</ButtonLink>
         <ButtonLink to="/meme" className="block sm:inline-block">View Random Meme</ButtonLink>
       </div>
     </div>
   );
 }
+
 
 export default HomeContent;
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom'; // 確保你使用了 react-router-dom
 
 function NavBar() {
   return (
-    <nav className="p-4 bg-gray-800 text-white">
-      <h1 className="text-2xl">My Funny Website</h1>
+    <nav className="bg-gray-800 p-4">
+      <div className="container mx-auto flex justify-between items-center">
+        <Link to="/" className="text-2xl text-white">My Funny Website</Link>
+        {/* 其他導航項目 */}
+      </div>
     </nav>
   );
 }
