f5523cb2dd427e506c4d61c72b12e8ec59f969f3
chihpao <chihpaoo@gmail.com>
2024-09-20 14:36:14 +0800

    修改css

--- src/pages/HomeContent.jsx
+++ src/pages/HomeContent.jsx
@@ -1,19 +1,10 @@
 import React from 'react';
+import '../styles/index.css'; // 引入 CSS 檔案
 
 function HomeContent() {
   return (
     <div className="w-full flex flex-col">
-      <div
-        className="relative"
-        style={{
-          height: '100vh',
-          backgroundImage: 'url(/tata02.jpg)',
-          backgroundSize: 'cover',
-          backgroundPosition: 'center top', // 調整背景位置
-          backgroundRepeat: 'no-repeat',
-          zIndex: -1 // 確保背景圖在 NavBar 之下
-        }}
-      >
+      <div className="background-image-container">
       </div>
     </div>
   );
