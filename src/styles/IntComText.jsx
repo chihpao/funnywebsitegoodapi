584bee5227a26e4313d1b48a40bad2572081f166
chihpao <chihpaoo@gmail.com>
2024-12-10 14:14:32 +0800

    動畫小改

--- src/styles/IntComText.jsx
+++ src/styles/IntComText.jsx
@@ -1,19 +1,51 @@
-// FILE: styles.jsx
-import { css } from '@emotion/react';
+// FILE: src/styles/IntComText.jsx
+import { Global, css } from '@emotion/react';
 
-export const moveAnimation = css`
-  @keyframes move {
-    0% {
-      transform: translateX(0);
-    }
-    50% {
-      transform: translateX(100px);
-    }
-    100% {
-      transform: translateX(0);
-    }
-  }
-  .animate-move {
-    animation: move 3s infinite;
-  }
-`;
+export const moveAnimation01 = (
+  <Global
+    styles={css`
+      @keyframes move {
+        0% {
+          transform: translateX(0);
+        }
+        50% {
+          transform: translateX(100px);
+        }
+        100% {
+          transform: translateX(0);
+        }
+      }
+      .animate-move {
+        animation: move 3s infinite;
+      }
+      @keyframes slide-up {
+        0%, 100% {
+          transform: translateY(100%);
+          opacity: 0;
+        }
+        20%, 40% {
+          transform: translateY(0);
+          opacity: 1;
+        }
+        60%, 80% {
+          transform: translateY(-100%);
+          opacity: 0;
+        }
+      }
+      .highlight {
+        display: inline-flex;
+        align-items: center;
+        justify-content: center;
+        border: 1px solid #000;
+        padding: 3px 6px;
+        background-color: transparent;
+        overflow: hidden;
+        height: 1em;
+      }
+
+      .highlight-content {
+        animation: slide-up 2.5s ease-in-out infinite;
+      }
+    `}
+  />
+);
