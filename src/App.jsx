2666c3ce73967afabd67cc4323539c7dfe554f17
chihpao <chihpaoo@gmail.com>
2024-12-18 16:00:05 +0800

    botpress機器人

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,15 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import MainContent from './pages/MainContent';
 import SkinnyFooter from './components/SkinnyFooter';
+import Botpress from "./components/Botpress"; // 確保路徑正確
+
 
 
 function App() {
   return (
     <Router>
         <div>
+          <Botpress />
           <NavBar />
           <MainContent />
           <SkinnyFooter />
