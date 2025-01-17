726501db6cffb047adbc49f642358f8de97b0d81
chihpao <chihpaoo@gmail.com>
2025-01-17 14:10:39 +0800

    防止自動翻譯出現

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,14 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import MainContent from './pages/MainContent';
 import SkinnyFooter from './components/SkinnyFooter';
-// import Botpress from "./components/Botpress";
+// import Botpress from "./components/Botpress"; // 確保路徑正確
 
 
 
 function App() {
   return (
     <Router>
-        <div>
+        <div translate="no">
           {/* <Botpress /> */}
           <NavBar />
           <MainContent />
--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './styles/index.css'
 
+
+const rootElement = document.getElementById('root');
+rootElement.className = 'notranslate';
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
