f051bc4c88a79f60d96c9128e7c509cb1235f23a
chihpao <chihpaoo@gmail.com>
2025-01-17 14:00:12 +0800

    skinny

--- src/components/SkinnyFooter.jsx
+++ src/components/SkinnyFooter.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SkinnyFooter = () => (
   <footer className="flex justify-center items-center w-full p-2 bg-white relative z-10">
-    <p>© 2024 小保. All rights reserved.</p>
+    <p>© 2025 小保. All rights reserved.</p>
   </footer>
 );
 
