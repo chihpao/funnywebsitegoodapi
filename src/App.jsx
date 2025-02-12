d3b62a15b6d0026b257ffca56843171919d4e7a2
chihpao <chihpaoo@gmail.com>
2025-02-12 15:58:28 +0800

    新增API頁面

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,43 @@
+// 語言: JavaScript (React)
+// 檔案位置: /src/App.jsx
 import React, { useState, useEffect } from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import MainContent from './pages/MainContent';
 import SkinnyFooter from './components/SkinnyFooter';
-// import Botpress from "./components/Botpress"; // 確保路徑正確
-
-
+import HomePage from './pages/HomePage';
+import FunComponent, { MemesPage, CatsPage, DogsPage } from './pages/FunComponent';
+import InteractiveComponent from './pages/InteractiveComponent';
+import API from './pages/API';
+import LoadingPage from './pages/LoadingPage';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingPage />;
+  }
+
   return (
     <Router>
-        <div translate="no">
-          {/* <Botpress /> */}
-          <NavBar />
-          <MainContent />
-          <SkinnyFooter />
-        </div>
+      <div translate="no">
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/fun" element={<FunComponent />} />
+          <Route path="/fun/memes" element={<MemesPage />} />
+          <Route path="/fun/cats" element={<CatsPage />} />
+          <Route path="/fun/dogs" element={<DogsPage />} />
+          <Route path="/interactive" element={<InteractiveComponent />} />
+          <Route path="/api" element={<API />} />
+        </Routes>
+        <SkinnyFooter />
+      </div>
     </Router>
   );
 }
---
+++ src/pages/API.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function API() {
+  return (
+    <div className="p-6">
+      <h1 className="text-4xl font-bold mb-4">API 說明</h1>
+      <p className="mb-4">
+        下列 API 提供 JSON 格式的資料，您可以根據以下網址存取相關資源：
+      </p>
+      <ul className="list-disc pl-5">
+        <li>隨機笑話: <a href="http://localhost:3000/api/joke" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/joke</a></li>
+        <li>搞笑梗圖: <a href="http://localhost:3000/api/memes" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/memes</a></li>
+        <li>貓咪圖片: <a href="http://localhost:3000/api/cats" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/cats</a></li>
+        <li>狗狗圖片: <a href="http://localhost:3000/api/dogs" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/dogs</a></li>
+      </ul>
+      <p className="mt-4">
+      </p>
+    </div>
+  );
+}
