4f5d322ae10ccd0382e1a64ddc686fd1016c15a0
chihpao <chihpaoo@gmail.com>
2025-02-21 17:35:49 +0800

    api9x

--- src/pages/apiPage.jsx
+++ src/pages/apiPage.jsx
@@ -8,10 +8,10 @@ export default function api() {
         下列 API 提供 JSON 格式的資料，您可以根據以下網址存取相關資源：
       </p>
       <ul className="list-disc pl-5">
-        <li>隨機笑話: <a href="http://localhost:3000/api/joke" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/joke</a></li>
-        <li>搞笑梗圖: <a href="http://localhost:3000/api/memes" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/memes</a></li>
-        <li>貓咪圖片: <a href="http://localhost:3000/api/cats" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/cats</a></li>
-        <li>狗狗圖片: <a href="http://localhost:3000/api/dogs" className="text-blue-600" target="_blank" rel="noreferrer">http://localhost:3000/api/dogs</a></li>
+        <li>隨機笑話: <a href="https://funnywebsitegoodapi.vercel.app/api/joke" className="text-blue-600" target="_blank" rel="noreferrer">https://funnywebsitegoodapi.vercel.app/api/joke</a></li>
+        <li>搞笑梗圖: <a href="https://funnywebsitegoodapi.vercel.app/api/memes" className="text-blue-600" target="_blank" rel="noreferrer">https://funnywebsitegoodapi.vercel.app/api/memes</a></li>
+        <li>貓咪圖片: <a href="https://funnywebsitegoodapi.vercel.app/api/cats" className="text-blue-600" target="_blank" rel="noreferrer">https://funnywebsitegoodapi.vercel.app/api/cats</a></li>
+        <li>狗狗圖片: <a href="https://funnywebsitegoodapi.vercel.app/api/dogs" className="text-blue-600" target="_blank" rel="noreferrer">https://funnywebsitegoodapi.vercel.app/api/dogs</a></li>
       </ul>
       <p className="mt-4">
       </p>
